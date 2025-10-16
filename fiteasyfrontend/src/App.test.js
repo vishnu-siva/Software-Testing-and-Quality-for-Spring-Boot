8cb75a54d0e081d7c6e7d660d779235899364325
vishnu-siva <vishnuhasivanandarajah8855@gmail.com>
2025-10-16 15:35:34 +0530

    commit 4,

--- fiteasyfrontend/src/App.test.js
+++ fiteasyfrontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders FitEase title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/FitEase/i);
+  expect(titleElement).toBeInTheDocument();
 });
