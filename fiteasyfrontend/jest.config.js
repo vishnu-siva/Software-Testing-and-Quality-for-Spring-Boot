1558496031d55b6ace21b5f8d404c7b2aad58521
vishnu-siva <vishnuhasivanandarajah8855@gmail.com>
2025-10-16 15:28:19 +0530

    commit3

--- fiteasyfrontend/jest.config.js
+++ fiteasyfrontend/jest.config.js
@@ -1,11 +1,17 @@
 module.exports = {
   testEnvironment: 'jsdom',
   transformIgnorePatterns: [
-    'node_modules/(?!(axios)/)'
+    '/node_modules/(?!(axios)/)',
+    '^.+\\.module\\.(css|sass|scss)$'
   ],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png|jpg|jpeg)$': '<rootDir>/__mocks__/fileMock.js'
   },
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js']
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/index.js',
+    '!src/reportWebVitals.js'
+  ]
 };
